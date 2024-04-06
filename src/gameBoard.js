d17dec7af8b399909a6a50515d68c9b0769da9a2
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-04-06 16:19:20 -0400

    Finish testing on the placeShip function

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,3 +1,5 @@
+// types of tiles
+
 const createTile = () => {
   let type = "empty";
   let ship = null;
@@ -11,31 +13,89 @@ const createGameBoard = () => {
   const getGrid = () => {
     return grid;
   };
-  const placeShip = (ship, coords, horizontal, player) => {
-    let placedShip = true;
-    const shipLen = ship.getSize();
 
+  const isValidSpot = (shipLen, coords, horizontal) => {
+    let valid = true;
     let x = coords[0];
     let y = coords[1];
 
     for (let i = 0; i < shipLen; i++) {
+      if (x > 9 || y > 9) return false;
+
       const currentTile = grid[y][x];
-      if (currentTile !== "E") {
-        placedShip = false;
+      if (!currentTile || currentTile.type !== "empty") {
+        valid = false;
         break;
       }
       if (horizontal) x += 1;
       else y += 1;
-
-      console.log({ x, y });
     }
+
+    return valid;
+  };
+
+  const getToBeShipTiles = (shipLen, coords, horizontal) => {
+    const returnArr = [];
+    let x = coords[0];
+    let y = coords[1];
+    for (let i = 0; i < shipLen; i++) {
+      returnArr.push(grid[y][x]);
+      if (horizontal) x += 1;
+      else y += 1;
+    }
+
+    return returnArr;
+  };
+
+  const getToBeUnavailableTiles = (shipLen, coords, horizontal) => {
+    const returnArr = [];
+    const dX = [0, 1, 1, 1, 0, -1, -1, -1];
+    const dY = [-1, -1, 0, 1, 1, 1, 0, -1];
+    let shipX = coords[0];
+    let shipY = coords[1];
+    for (let i = 0; i < shipLen; i++) {
+      for (let j = 0; j < 8; j++) {
+        const x = shipX + dX[j];
+        const y = shipY + dY[j];
+        if (x > -1 && x < 10 && y > -1 && y < 10 && grid[y][x].type === "empty" && !returnArr.includes(grid[y][x]))
+          returnArr.push(grid[y][x]);
+      }
+      if (horizontal) shipX += 1;
+      else shipY += 1;
+    }
+
+    return returnArr;
+  };
+  const placeShip = (ship, coords, horizontal) => {
+    let placedShip = isValidSpot(ship.getSize(), coords, horizontal);
+
+    if (placedShip) {
+      const shipTiles = getToBeShipTiles(ship.getSize(), coords, horizontal);
+      shipTiles.forEach((tile) => {
+        tile.type = "ship";
+        tile.ship = ship;
+      });
+
+      const unavailableTiles = getToBeUnavailableTiles(
+        ship.getSize(),
+        coords,
+        horizontal
+      );
+      unavailableTiles.forEach((tile) => (tile.type = "unavailable"));
+
+    }
+
     return placedShip;
   };
-  const fireShot = () => {};
+  const fireShot = (coords, ) => {
+    // takes 
+  };
   const allShipsSunk = () => {};
 
   return { getGrid, placeShip, fireShot, allShipsSunk };
 };
 
-console.log(createGameBoard().getGrid());
+// const test = createGameBoard();
+// console.log(test.isValidSpot(3, [9, 8], false));
+
 export { createGameBoard };
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,6 +1,94 @@
 import { createGameBoard } from "../gameBoard";
 
-test("createGameBoard.placeShip()")
+test(".fireShot() ")
+test(".placeShip() returns false when placing on another ship", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeTwo = { getSize: () => 2 };
+  const shipSizeOne = { getSize: () => 1 };
+  gameBoard.placeShip(shipSizeTwo, [0, 0], true);
+  expect(gameBoard.placeShip(shipSizeOne, [0, 0], true)).toBe(false);
+});
+
+test(".placeShip() returns false when placing on unavailable spot", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeTwo = { getSize: () => 2 };
+  const shipSizeOne = { getSize: () => 1 };
+  gameBoard.placeShip(shipSizeTwo, [0, 0], true, null);
+  expect(gameBoard.placeShip(shipSizeOne, [0, 1], true, null)).toBe(false);
+});
+test(".placeShip(), placed horizontally, assigns the correct tiles to unavailable", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeTwo = { getSize: () => 2 };
+  gameBoard.placeShip(shipSizeTwo, [1, 6], true, null);
+  expect(gameBoard.getGrid()[7][0].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[7][1].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[7][2].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[7][3].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[5][0].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[5][1].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[5][2].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[5][3].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[6][3].type).toBe("unavailable");
+  expect(gameBoard.getGrid()[6][3].type).toBe("unavailable");
+});
+test(".placeShip() correctly assigns the ship", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeThree = { getSize: () => 3 };
+  gameBoard.placeShip(shipSizeThree, [4, 7], true, shipSizeThree);
+  expect(gameBoard.getGrid()[7][4].ship).toBe(shipSizeThree);
+  expect(gameBoard.getGrid()[7][5].ship).toBe(shipSizeThree);
+  expect(gameBoard.getGrid()[7][5].ship).toBe(shipSizeThree);
+});
+
+test(".placeShip() sets the correct tiles to be type ship and assigns the ship to the right tiles", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeThree = { getSize: () => 3 };
+  gameBoard.placeShip(shipSizeThree, [4, 7], true, null);
+  expect(gameBoard.getGrid()[7][4].type).toBe("ship");
+  // expect(gameBoard.getGrid()[7][4].ship).toBe(shipSizeThree);
+
+  expect(gameBoard.getGrid()[7][5].type).toBe("ship");
+  // expect(gameBoard.getGrid()[7][5].ship).toBe(shipSizeThree);
+
+  expect(gameBoard.getGrid()[7][5].type).toBe("ship");
+  // expect(gameBoard.getGrid()[7][5].ship).toBe(shipSizeThree);
+});
+test(".placeShip() returns returns false when the coords are out of bounds", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeThree = { getSize: () => 3 };
+  expect(gameBoard.placeShip(shipSizeThree, [10, 10], false, null)).toBe(false);
+});
+test(".placeShip() returns true when placed in a valid spot vertically", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeThree = { getSize: () => 3 };
+  expect(gameBoard.placeShip(shipSizeThree, [0, 0], false, null)).toBe(true);
+});
+test(".placeShip() returns false when placed on an invalid spot vertically", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeThree = { getSize: () => 3 };
+  expect(gameBoard.placeShip(shipSizeThree, [0, 9], false, null)).toBe(false);
+});
+test("createGameBoard.placeShip() returns true when placed in valid spot on empty grid horizontally", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeThree = { getSize: () => 3 };
+  expect(gameBoard.placeShip(shipSizeThree, [0, 0], true, null)).toBe(true);
+});
+
+test("createGameBoard.placeShip() returns false when placed on invalid spot on empty grid horizontally", () => {
+  const gameBoard = createGameBoard();
+  // mock ship
+  const shipSizeThree = { getSize: () => 3 };
+  expect(gameBoard.placeShip(shipSizeThree, [9, 9], true, null)).toBe(false);
+});
 
 test("createGameBoard.getGrid() returns a 10x10 grid", () => {
   const gameBoard = createGameBoard();
@@ -12,6 +100,19 @@ test("createGameBoard.getGrid() returns a 10x10 grid", () => {
   });
 });
 
+test("createGameBoard returns an OBJ with getGrid, placeShip, fireShot, allShipsSunk, methods", () => {
+  expect(createGameBoard(1).getGrid).toBeDefined();
+  expect(createGameBoard(1).getGrid).toBeInstanceOf(Function);
+
+  expect(createGameBoard(1).placeShip).toBeDefined();
+  expect(createGameBoard(1).placeShip).toBeInstanceOf(Function);
+
+  expect(createGameBoard(1).fireShot).toBeDefined();
+  expect(createGameBoard(1).fireShot).toBeInstanceOf(Function);
+
+  expect(createGameBoard(1).allShipsSunk).toBeDefined();
+  expect(createGameBoard(1).allShipsSunk).toBeInstanceOf(Function);
+});
 test("createGameBoard exists", () => {
   expect(createGameBoard).toBeDefined();
 });
