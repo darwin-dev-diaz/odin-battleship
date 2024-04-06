b57d3534f8285e83ebd3bd814f8d5f6db6073385
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-04-06 14:11:13 -0400

    Add createTile factory

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,21 +1,41 @@
+const createTile = () => {
+  let type = "empty";
+  let ship = null;
+  return { type, ship };
+};
+
 const createGameBoard = () => {
-  const grid = Array.from({ length: 10 }, () => [
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-  ]);
+  const grid = Array.from({ length: 10 }, () =>
+    Array.from({ length: 10 }, () => createTile())
+  );
   const getGrid = () => {
     return grid;
   };
+  const placeShip = (ship, coords, horizontal, player) => {
+    let placedShip = true;
+    const shipLen = ship.getSize();
 
-  return { getGrid };
+    let x = coords[0];
+    let y = coords[1];
+
+    for (let i = 0; i < shipLen; i++) {
+      const currentTile = grid[y][x];
+      if (currentTile !== "E") {
+        placedShip = false;
+        break;
+      }
+      if (horizontal) x += 1;
+      else y += 1;
+
+      console.log({ x, y });
+    }
+    return placedShip;
+  };
+  const fireShot = () => {};
+  const allShipsSunk = () => {};
+
+  return { getGrid, placeShip, fireShot, allShipsSunk };
 };
 
+console.log(createGameBoard().getGrid());
 export { createGameBoard };
