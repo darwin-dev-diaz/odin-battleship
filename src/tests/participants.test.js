550d3a3dd6567d8838dd4ebe128adeb2adf6517e
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-04-08 14:22:20 -0400

    Make sure the participant module passes tests

--- src/tests/participants.test.js
+++ src/tests/participants.test.js
@@ -1,5 +1,12 @@
 import { createComputerPlayer, createHumanPlayer } from "../participants.js";
 
-test("Can this just fucking working", ()=>{
-  console.log()
-})
+
+
+test("createHumanPlayer is able to place all ten ships", ()=>{
+  expect(createHumanPlayer().gameBoard.placeShip(undefined, [0,0], true)).toBe(true);
+});
+
+test("createComputer and createPlayer methods exists", ()=>{
+  expect(createComputerPlayer).toBeDefined();
+  expect(createHumanPlayer).toBeDefined();
+});
